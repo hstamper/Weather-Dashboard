70f5e60f5e6f5c3b8b10a0bc93a0ed60541f0165
Hilliard Stamper <hilliard.stamper@att.net>
2021-11-21 17:47:18 -0500

    added fetch request for IV Index data

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var searchHistory = []
 var lastCitySearched = ""
 
 var getCityWeather = function(city) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=jacksonville,us&appid=f4401833be8e217e5268c0b70c5108c4&units=imperial";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=f4401833be8e217e5268c0b70c5108c4&units=imperial";
 
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
@@ -38,7 +38,7 @@ var displayWeather = function(weatherData) {
     $("#main-city-name").text(weatherData.name + " (" + dayjs(weatherData.dt * 1000).format("MM/DD/YYYY") + ") ").append(`<img src="https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png"></img>`);
     $("#main-city-temp").text("Tempature: " + weatherData.main.temp.toFixed(1) + "°F");
     $("#main-city-humid").text("Humidity: " + weatherData.main.humidity + "%");
-    $("#main-city-wind").text("Wind Speed: " + weatherData.wind.speed.toFixed(i) + " mph"); 
+    $("#main-city-wind").text("Wind Speed: " + weatherData.wind.speed.toFixed(1) + " mph"); 
 
     fetch("https://api.openweathermap.org/data/2.5/uvi?lat=" + weatherData.coord.lat + "&lon="+ weatherData.coord.lon + "&appid=f4401833be8e217e5268c0b70c5108c4")
     .then(function(response) {
@@ -58,6 +58,29 @@ var displayWeather = function(weatherData) {
             }
         })
     });
+
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + weatherData.name + "&appid=f4401833be8e217e5268c0b70c5108c4&units=imperial")
+    .then(function(response) {
+        response.json().then(function(data) {
+            $("#five-day").empty(); 
+
+            for(i = 7; i<= data.list.length; i += 8) {
+                
+                var fiveDayCard = `
+                <div class="col-md-2 py-3 card text-white bg-primary">
+                <div class="card-body p-1">
+                    <h5 class="card-title">` + dayjs(data.list[i].dt * 1000).format("MM/DD/YYYY") + `</h5>
+                    <img src="https://openweathermap.org/img/wn/` + data.list[i].weather[0].icon + `.png" alt="rain">
+                    <p class="card-text">Temp: ` + data.list[i].main.temp + `</p>
+                    <p class="card-text">Humidity: ` + data.list[i].main.humidity + `</p>
+                </div>
+            </div>
+            `;
+
+            $("#five-day").append(fiveDayCard);
+            }
+        })
+    })
 
 
 }
